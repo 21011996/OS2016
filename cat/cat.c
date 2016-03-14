4d3d56d005801e656f4455760c85e6839a4e6b71
Ilya239 <21011996@mail.ru>
2016-03-15 02:07:38 +0300

    Simplier one

--- cat/cat.c
+++ cat/cat.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-ssize_t read_(int fd, void *buf, size_t counter) {
-    ssize_t total_read = 0;
-    ssize_t byte_read = 0;
-    while (total_read < counter) {
-        byte_read = read(fd, buf + total_read, counter - total_read);
-        if (byte_read <= 0) {
-            break;
-        } else {
-            total_read += byte_read;
-        }
-    }
-    if (byte_read < 0) {
-        return -1;
-    }
-    return total_read;
-}
-
 ssize_t write_(int fd, const void *buf, size_t counter) {
     ssize_t total_written = 0;
     ssize_t bytes_written = 0;
@@ -35,18 +18,15 @@ ssize_t write_(int fd, const void *buf, size_t counter) {
     return total_written;
 }
 
-int main(int argc, char * argv[]) {
+int main() {
 
     char buffer[1024];
     while (1) {
-        ssize_t bytes_read = read_(STDIN_FILENO, buffer, 1024);
-        if (bytes_read < 0) {
-            break;
-        }
-        write_(STDOUT_FILENO, buffer, bytes_read);
+        ssize_t bytes_read = read(0, buffer, 1024);
         if (bytes_read == 0) {
             break;
         }
+        write_(STDOUT_FILENO, buffer, bytes_read);
     }
 
     return 0;

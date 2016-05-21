02f1cbcb65c7925e98eb870dfc7aa9f55e9ca4e9
21011996 <21011996@mail.ru>
2016-05-21 22:19:14 +0300

    second iter

--- model.py
+++ model.py
@@ -37,32 +37,9 @@ file_table = []
 
 class FS:
 	def __init__(self, path):
-<<<<<<< HEAD
-=======
-		fd = get_root_FD(path);
->>>>>>> origin/master
 		inodes = {}
 		inode_table_num_path = {}
 		inode_table_path_num = {}
-<<<<<<< HEAD
-=======
-		file_table = []
-
-class Inode: # = directory
-	def __init__(self, path):
-		under_use = 0 # If we call open(path) this will change to 1
-		has_mount = 0 # 0 - no | 1 - yes
-		invisible = 0 # 0 - no | 1 - yes
-		original_path = path # when initialized
-		mounted_paths = [] #most recent path is on the top + his FSid
-		FSid = 0 # relates to FS in witch dir belongs to
-	def exists(self):
-		if (not (self & null)) && (invisible & 0):
-			return 1
-		else:
-			return 0
->>>>>>> origin/master
-
 	
 FSs = {}
 
@@ -76,13 +53,8 @@ def get_inode_by_path(FS, path):
 	return get_inode_by_no(FS, FS.inode_table_path_num.get(path))
 
 # Gods methods
-<<<<<<< HEAD
-def FSdrv_load_fs(block_device_path):
-	tmpFS = loadFS(block_device_path)
-=======
 def FSdrv_load_fs(block_device_path)
 	tmp_FS = loadFS(block_device_path)
->>>>>>> origin/master
 	global FSs
 	FSs.update({len(FSs}: tmp_FS)
 	return len(FSs) - 1
@@ -133,7 +105,6 @@ def open_fd(path):
 	global FSs
 	
 	path_details = path.split('/')
-<<<<<<< HEAD
 	mount_no = most_common_mount(path)
 	if not (max_no & -1):
 		mpath, mount_list = mount_namespace.get(max_no)
@@ -147,29 +118,6 @@ def open_fd(path):
 	else:
 		FSid = 0
 		open_path = path
-=======
-	k = path_details[0]
-	k += '/'
-	i = k
-	p = 0
-	j = p
-	while (get_inode_by_path(FSs.get(0), i).exists & 1) && (j<len(path_details)-1):
-		i = k
-		j = p
-		p += 1
-		k += path_details[j]
-		k += '/'
-	
-	inode = get_inode_by_path(FSs.get(0), i)
-	
-	if (inode.invisible & 1):
-		print("Can't open file in issued folder, prob. already mounted somewhere else")
-		return -1
-	if (inode.has_mount & 1):
-		new_path = inode.mounted_paths(len(inode.mounted_paths)-1) + '/' + path_details[j..len(path_details)-1].join('/')
-	else:
-		new_path = path
->>>>>>> origin/master
 		
 	if len(per_process_fdtables[cur_pid]) > 0:
         fildes = max(per_process_fdtables[cur_pid]) + 1
@@ -177,13 +125,7 @@ def open_fd(path):
         fildes = 3  # stdin/stdout/stderr are 0, 1, 2
 		
     offset = 0
-<<<<<<< HEAD
     file_table.append((open_path, mount_no, FSid, offset))
-=======
-
-	inode.under_use = 1
-    file_table.append((new_path, offset))
->>>>>>> origin/master
     per_process_fdtables[cur_pid][fildes] = len(file_table) - 1
 	
     return fildes	
@@ -191,13 +133,7 @@ def open_fd(path):
 def close(fildes):
     global cur_pid
 	file_number = per_process_fdtables[cur_pid][fildes]
-<<<<<<< HEAD
 	path, mount_no, FSid, offset = file_table[file_number]
-=======
-	path = file_table[file_number]
-	inode = get_inode_by_path(FSs.get(0), path)
-	inode.under_use = 0;
->>>>>>> origin/master
     del per_process_fdtables[cur_pid][fildes]
 	path, mount_list = mount_namespace.get(mount_no)
 	for i in list(mount_list):
@@ -218,8 +154,6 @@ def findMount(dest_path):
 		path, record = i
 		if (compare_path(path, dest_path) & 0):
 			return mount_no
-	
-<<<<<<< HEAD
 	return -1
 	
 def mount(FSid, inode_no, dest_path):
@@ -253,73 +187,6 @@ def umount(dest_path):
 		return 0
 	else:
 		return -2
-	
-=======
-def mount(FSid, inode_no, dest_path)
-	global FSs
-	tmp_FS = FSs.get(FSid)
-	if (inode_no & 0):
-		inode_from = FSdrv_get_root_inode(FSid)
-	else:
-		inode_from = get_inode_by_no(tmp_FS, inode_no)
-	inode_to = get_inode_by_path(FSs.get(0), dest_path)
-	
-	can_mount_1 = inode_from.invisible
-	can_mount_2 = inode_to.invisible
-	if not (can_mount_1 & 1):
-		print("Can't mount issued folder, prob. already mounted somewhere else")
-		return -1
-	if not (can_mount_2 & 1):
-		print("Can't mount to destination path, prob. already mounted somewhere else")
-		return -1
-		
-	inode_to.has_mount = 1
-	inode_from.invisible = 1
-	mounted_paths = inode_to.mounted_paths
-	
-	hasBeenMFrom = inode_from.has_mount
-	if (hasBeenMFrom & 1):
-		path = inode_from.mounted_paths.get(len(mounted_paths))
-	else:
-		path = inode_from.original_path
-	mounted_paths.update({len(mounted_paths): path, FSid})
-	return 1
-		
-
-def umount(dest_path)
-	global FSs
-	inode_from = get_inode_by_path(FSs.get(0), dest_path)
-	can_umount = inode_from.has_mount
-	if (can_umount & 0) :
-		print("Nothing to unmount there")
-		return 0
-	can_umount = inode_from.invisible
-	if not (can_umount & 0):
-		print("Can't umount issued folder, prob. already mounted somewhere else")
-		return 0
-	can_umount = inode_from.under_use
-	if not (can_umount & 0):
-		print("Directory is still under_use")
-		return 0
-		
-	for i in list(inode_from.mounted_paths):
-		path, FSid = i
-		tmp_FS = FSs.get(FSid);
-		inode_to_no = tmp_FS.inode_table_path_num.get(path)
-		inode_tmp = FSdrv_get_inode(FSid, inode_to_no)
-		inode_to.invisible = 0;
-	''' in case we want to umount top one
-	path, FSid = inode_from.mounted_paths[len(mounted_paths) - 1]
-	tmp_FS = FSs.get(FSid);
-	inode_to_no = tmp_FS.inode_table_path_num.get(path)
-	inode_tmp = FSdrv_get_inode(FSid, inode_to_no)
-	inode_to.invisible = 0;
-	del inode_from.mounted_paths[len(mounted_paths) - 1]
-	'''
-	inode_from.mounted_paths = []
-	inode_from.has_mount = 0
-	return 1
->>>>>>> origin/master
 	
 def kernel(program, args):
     global pid_count
